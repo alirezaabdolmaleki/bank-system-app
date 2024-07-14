66753d29024c752c30042911937e95a4ce9b2aa3
Alireza abdolmaleki <alirezaabdolmaleki9080.com>
2024-07-15 01:03:09 +0330

    add models

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,26 +20,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'mobile_number',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
 }
