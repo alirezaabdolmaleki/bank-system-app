46a29b3c9a2faa5be88fedfba13a3cef60dda258
Alireza abdolmaleki <alirezaabdolmaleki9080.com>
2024-07-15 23:31:51 +0330

    add language for app and change local time

---
+++ lang/en/payment.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Payment Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'success' => [
+        "sender" => 'Your transfer was successful. \r\nTransaction Id: :id',
+        "reciver" => 'You have received a transfer. \r\nTransaction Id: :id',
+
+    ],
+
+    "error" => 'Your transfer failed: :error'
+
+];
